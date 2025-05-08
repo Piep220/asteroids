f352b0a9fa09bcf4abe4c634dc9eb85a7eb01f6f
Piep220 <pfallon05@gmail.com>
2025-05-09 00:47:52 +0100

    CH2 L8 grouping

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ from constants import *
 from player import Player
 
 def main():
+    ## GAME INIT ################################
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    ## GROUPS ###################################
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    ## TIME STUFF ###############################
     dt = 0
     game_clock = pygame.time.Clock()
+
+    ## DISPLAY SETUP ############################
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -20,16 +30,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        ## GAMEPLAY #######################################
+        updatable.update(dt)
+
         ## DRAW CALLS #####################################
         screen.fill("black")
-        player.draw(screen)
-
+        for obj in drawable:
+            obj.draw(screen)
 
         ## FRAME RATE LOGIC ###############################
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
         #print(dt)
-
 
 
 if __name__ == "__main__":
