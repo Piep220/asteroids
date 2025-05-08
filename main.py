16f1eade12e7a31a7da597a54197afcc2b318f36
Piep220 <pfallon05@gmail.com>
2025-05-09 00:00:35 +0100

    CH2 L5 - add plaer and screen draw

--- main.py
+++ main.py
@@ -3,20 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
+    game_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    ## MAIN GAME LOOP ###############################################
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        ## DRAW CALLS #####################################
         screen.fill("black")
-        #pygame.Surface.fill(screen, pygame.Color("black"))
+        player.draw(screen)
+
+
+        ## FRAME RATE LOGIC ###############################
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        #print(dt)
 
 
 
