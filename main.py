41472935339d304f011e30f31cd86028e2101466
Piep220 <pfallon05@gmail.com>
2025-05-10 11:47:14 +0100

    CH3 L1 add asteroid + field

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+## BASE IMPORTS #################################
 import pygame
 from constants import *
+
+## CLASS IMPORTS ################################
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     ## GAME INIT ################################
@@ -11,18 +16,22 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    ## GROUPS ###################################
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    
+    ## DISPLAY SETUP ############################
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     ## TIME STUFF ###############################
     dt = 0
     game_clock = pygame.time.Clock()
+    ## GROUPS/CONTAINERS ########################
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    ## DISPLAY SETUP ############################
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    ## GAME SETUP ###############################
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     ## MAIN GAME LOOP ###############################################
